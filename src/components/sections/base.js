eb4455fe95f8f5f47a9c4b55de477710ddf745b6
Ilias Trichopoulos <hlias.nop@gmail.com>
2021-03-12 12:53:13 +0100

    Fix undefined HTML class

--- src/components/sections/base.js
+++ src/components/sections/base.js
@@ -2,12 +2,12 @@ import React from "react"
 
 import styles from "./base.module.scss"
 
-const BaseSection = ({ id, children, className, noTopPadding }) => {
+const BaseSection = ({ id, children, className = "", noTopPadding }) => {
   return (
     <section
       id={id}
-      className={`global-content-wrapper ${className || ""} ${
-        noTopPadding && styles.noTopPadding
+      className={`global-content-wrapper ${className} ${
+        noTopPadding ? styles.noTopPadding : ""
       }`}
     >
       {children}
