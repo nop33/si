7e2f7337f496a424c60ba25933a5e5f912546bd9
Ilias Trichopoulos <hlias.nop@gmail.com>
2022-10-23 11:23:12 +0200

    Open external links in new tab

--- gatsby-config.js
+++ gatsby-config.js
@@ -86,6 +86,7 @@ module.exports = {
           `gatsby-remark-prismjs`,
           `gatsby-remark-copy-linked-files`,
           `gatsby-remark-smartypants`,
+          `gatsby-remark-external-links`,
         ],
       },
     },
