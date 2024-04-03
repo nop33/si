f7b436af2f2e29b39541ad57779972ef9db4713a
Ilias Trichopoulos <hlias.nop@gmail.com>
2024-04-03 22:41:44 +0900

    Integrate Algolia search

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,3 +1,5 @@
+require("dotenv").config()
+
 const mediaMaxWidth = 704
 
 module.exports = {
@@ -169,5 +171,13 @@ module.exports = {
     },
     `gatsby-plugin-preload-fonts`,
     `gatsby-plugin-styled-components`,
+    {
+      resolve: `gatsby-plugin-algolia`,
+      options: {
+        appId: process.env.GATSBY_ALGOLIA_APP_ID,
+        apiKey: process.env.ALGOLIA_ADMIN_KEY,
+        queries: require("./src/utils/algolia-queries"),
+      },
+    },
   ],
 }
---
+++ src/components/search/search-box.tsx
@@ -0,0 +1,35 @@
+import React from "react"
+import { useSearchBox } from "react-instantsearch"
+import { Search as SearchIcon } from "@styled-icons/fa-solid"
+
+const SearchBox = ({
+  className,
+  onFocus,
+  onChange,
+}: {
+  className?: string
+  onFocus: () => void
+  onChange: (string) => void
+}) => {
+  const { query, refine } = useSearchBox()
+
+  return (
+    <form className={className}>
+      <input
+        className="SearchInput"
+        type="text"
+        placeholder="Search"
+        aria-label="Search"
+        onChange={e => {
+          refine(e.target.value)
+          onChange(e.target.value)
+        }}
+        value={query}
+        onFocus={onFocus}
+      />
+      <SearchIcon className="SearchIcon" />
+    </form>
+  )
+}
+
+export default SearchBox
